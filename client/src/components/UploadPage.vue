a5da3b7bd7bd01d4d6f2ac27a444d6427449b5d3
Andrius Palivonas <andrius@palivonas.lt>
2017-12-02 23:08:55 +0000

    Implement upload page

--- client/src/components/UploadPage.vue
+++ client/src/components/UploadPage.vue
@@ -1,32 +1,98 @@
 <template>
-  <input type="file" />
+  <div>
+    <h2>Upload a movie list to the Moodslider database</h2>
+    <p>JSON and XML formats are supported</p>
+    <label class="upload-button" for="movieFileUpload" tabindex="0" :disabled="isUploading">{{isUploading ? 'Uploading...' : 'Select a file'}}</label>
+    <div class="message message--success" v-if="isSuccess">Database has been updated! Feel free to try the sliders now.</div>
+    <div class="message message--error" v-else-if="isError">{{ errorMessage || 'Unexpected error' }}</div>
+
+    <form enctype="multipart/form-data" novalidate>
+      <input @change="fileChanged($event.target.files[0])" type="file" id="movieFileUpload" accept=".xml, .json" />
+    </form>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'UploadPage',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-    };
-  },
-};
+  import * as axios from 'axios';
+
+  const uploadUrl = 'http://localhost:3001/movies';
+  const STATUS_UPLOADING = 'uploading';
+  const STATUS_SUCCESS = 'success';
+  const STATUS_ERROR = 'error';
+
+  export default {
+    name: 'UploadPage',
+    computed: {
+      isSuccess() {
+        return this.status === STATUS_SUCCESS;
+      },
+      isUploading() {
+        return this.status === STATUS_UPLOADING;
+      },
+      isError() {
+        return this.status === STATUS_ERROR;
+      },
+    },
+    data() {
+      return {
+        status: null,
+        errorMessage: null,
+        file: null,
+      };
+    },
+    methods: {
+      fileChanged(file) {
+        if (!file) {
+          this.status = null;
+          return;
+        }
+        if (!/(\.xml|\.json)/i.test(file.name)) {
+          this.status = STATUS_ERROR;
+          this.errorMessage = 'Invalid file';
+          return;
+        }
+        this.status = STATUS_UPLOADING;
+        this.upload(file)
+          .then(() => {
+            this.status = STATUS_SUCCESS;
+          })
+          .catch((err) => {
+            this.status = STATUS_ERROR;
+            this.errorMessage = err.response;
+          });
+      },
+
+      upload(file) {
+        const form = new FormData();
+        form.append('movieList', file);
+        return axios.post(uploadUrl, form);
+      },
+    },
+  };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+  #movieFileUpload {
+    display: none;
+  }
+  .upload-button {
+    font-size: 1.6em;
+    text-transform: uppercase;
+    padding: 3px 5px;
+    background: rgba(white, 0.6);
+    border-radius: 2px;
+    cursor: pointer;
+
+    &:hover {
+      background: rgba(white, 0.8);
+    }
+  }
+  .message {
+    &--success {
+      color: lime;
+    }
+    &--error {
+      color: red;
+    }
+  }
 </style>
