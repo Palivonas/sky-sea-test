f715f7067fe8038aa8db4dfd3a242a358cffd4d7
Andrius Palivonas <andrius@palivonas.lt>
2017-12-03 20:16:42 +0000

    Move private uploadMovieHandler methods outside the class

--- server/src/uploadMoviesHandler.js
+++ server/src/uploadMoviesHandler.js
@@ -1,27 +1,27 @@
+function fileNameValid(name) {
+  return /(\.xml|\.json)$/i.test(name);
+}
+
+function parseFile(file) {
+  if (/\.xml$/i.test(file.name)) {
+    return parseXml(file.data.toString());
+  }
+  return JSON.parse(file.data.toString());
+}
+
 class UploadMoviesHandler {
   constructor(store) {
     this.store = store;
   }
 
-  static fileNameValid(name) {
-    return /(\.xml|\.json)$/i.test(name);
-  }
-
-  static parseFile(file) {
-    if (/\.xml$/i.test(file.name)) {
-      return parseXml(file.data.toString());
-    }
-    return JSON.parse(file.data.toString());
-  }
-
   async handleRequest(req, res) {
     const file = req.files.movieList;
-    if (!file || !this.constructor.fileNameValid(file.name)) {
+    if (!file || !fileNameValid(file.name)) {
       res.status(422);
       res.send({ message: 'xml/json file required' });
       return;
     }
-    const movies = this.constructor.parseFile(file);
+    const movies = parseFile(file);
     try {
       await this.store.insertMovies(movies, { flushExisting: !!req.query.flushExisting });
       res.send({ message: 'Movies uploaded' });
