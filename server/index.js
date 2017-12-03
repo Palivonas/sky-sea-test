0e5e60bab881b8991d7766365544d3212ed4d6b2
Andrius Palivonas <andrius@palivonas.lt>
2017-12-03 20:16:09 +0000

    Refactor get movie handler to be reusable

--- server/index.js
+++ server/index.js
@@ -4,20 +4,21 @@ const fileUpload = require('express-fileupload');
 const path = require('path');
 
 const getMoviesHandler = require('./src/getMoviesHandler');
-const UploadMoviesHandler = require('./src/uploadMoviesHandler');
+const uploadMoviesHandler = require('./src/uploadMoviesHandler');
 const FileStore = require('./src/fileStore');
 
-const store = new FileStore(path.join(__dirname, 'data/movieDatabase.json'));
-
 const PORT = process.env.PORT || 3001;
-const IMAGE_DIR = path.join(__dirname, 'data/images');
+const IMAGE_DIR = process.env.IMAGE_DIR || path.join(__dirname, 'data/images');
+const JSON_STORE_PATH = process.env.STORE_FILE || path.join(__dirname, 'data/movieDatabase.json');
+
+const store = new FileStore(JSON_STORE_PATH);
 
 const app = express();
 app.use(cors());
 app.use(fileUpload());
 
 app.get('/movies', getMoviesHandler(store));
-app.post('/movies', UploadMoviesHandler(store));
+app.post('/movies', uploadMoviesHandler(store));
 app.use('/images', express.static(IMAGE_DIR));
 
 app.listen(PORT, () => {
--- server/src/getMoviesHandler.js
+++ server/src/getMoviesHandler.js
@@ -1,11 +1,10 @@
-let store;
-
 function getFitness(movie, userMoods) {
   if (movie.moodsExclude) {
     if (movie.moodsExclude.find(moodId => userMoods.find(mood => mood.id === moodId))) {
       return 0;
     }
   }
+
   return userMoods
     .map((mood) => {
       const indexInMovie = movie.moods.indexOf(mood.id);
@@ -28,33 +27,39 @@ function getMoviesByMood(movies, userMoods) {
     .sort((a, b) => ((b.fitness - a.fitness) || Math.random() - 0.5));
 }
 
-async function getMoviesHandler(req, res) {
-  if (!req.query.moods || !req.query.moods.length) {
-    res.status(422);
-    res.send({ message: 'Mood list required' });
+class GetMoviesHandler {
+  constructor(store) {
+    this.store = store;
   }
 
-  const moods = req.query.moods.map((moodJson) => {
-    const mood = JSON.parse(moodJson);
-    return {
-      id: String(mood.id),
-      value: Math.min(Math.abs(parseFloat(mood.value)), 1),
-    };
-  });
+  async handleRequest(req, res) {
+    if (!req.query.moods || !req.query.moods.length) {
+      res.status(422);
+      res.send({ message: 'Mood list required' });
+    }
 
-  const limit = Math.min(Math.abs(parseInt(req.query.limit, 10)) || 5, 20);
-  try {
-    const movies = await store.getMovies();
-    const moviesByMood = getMoviesByMood(movies, moods).slice(0, limit);
-    res.send({ movies: moviesByMood });
-  } catch (err) {
-    res.status(500);
-    res.send({ message: 'Unknown error' });
-    console.error('Failed to retrieve movies:', req.query, err);
+    const moods = req.query.moods.map((moodJson) => {
+      const mood = JSON.parse(moodJson);
+      return {
+        id: String(mood.id),
+        value: Math.min(Math.abs(parseFloat(mood.value)), 1),
+      };
+    });
+
+    const limit = Math.min(Math.abs(parseInt(req.query.limit, 10)) || 5, 20);
+    try {
+      const movies = await this.store.getMovies();
+      const moviesByMood = getMoviesByMood(movies, moods).slice(0, limit);
+      res.send({ movies: moviesByMood });
+    } catch (err) {
+      res.status(500);
+      res.send({ message: 'Unknown error' });
+      console.error('Failed to retrieve movies:', req.query, err);
+    }
   }
 }
 
-module.exports = (storeObject) => {
-  store = storeObject;
-  return getMoviesHandler;
+module.exports = (store) => {
+  const handler = new GetMoviesHandler(store);
+  return handler.handleRequest.bind(handler);
 };
